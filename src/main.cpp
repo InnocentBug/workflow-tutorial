55e4e52c75c8239c44b78dc29649cc4b26930f61
Ludwig Schneider <ludwigschneider@uchicago.edu>
2023-05-09 08:29:07 -0500

    silent trunk format

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,18 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]){
-  if(argc <= 1){
-    cerr<<"Usage: ./SUM number1 [number2, [number3 ...]]"<<endl;
+int main(int argc, char *argv[]) {
+  if (argc <= 1) {
+    cerr << "Usage: ./SUM number1 [number2, [number3 ...]]" << endl;
     return -1;
   }
 
-  double sum=0;
-  for(int i=1; i < argc; i++){
+  double sum = 0;
+  for (int i = 1; i < argc; i++) {
     const double value = stod(argv[i]);
     sum += value;
   }
-  cout<<sum<<endl;
-
+  cout << sum << endl;
 
   return 0;
 }
