2408ad039e596598a3661cde0737ae1d5a19e915
Ludwig Schneider <ludwigschneider@uchicago.edu>
2023-05-09 08:11:51 -0500

    add little c++ program

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+  if(argc <= 1){
+    cerr<<"Usage: ./SUM number1 [number2, [number3 ...]]"<<endl;
+    return -1;
+  }
+
+  double sum=0;
+  for(int i=1; i < argc; i++){
+    const double value = stod(argv[i]);
+    sum += value;
+  }
+  cout<<sum<<endl;
+
+
+  return 0;
+}
